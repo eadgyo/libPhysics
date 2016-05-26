8e1096909ec238495ac6b60c47ef665def04ecdf
ronan <ronanjamet.h@gmail.com>
2016-05-27 01:10:04 +0200

    Integration

--- build.sh
+++ build.sh
@@ -1,6 +1,28 @@
+#!/bin/bash
 mvn install
-if [ ! -d "build" ]; then
-    mkdir build
+
+name=$(basename target/*.jar)
+group=${name%%-*}
+idname=${name%-*}
+version=${name##*-}
+version=${version%.*}
+
+echo "Jar in $group/$idname/$version"
+
+if [ ! -d "$group" ]
+then
+    mkdir $group
 fi
 
-cp target/*.jar build
+if [ ! -d "$group/$idname" ]
+then
+    mkdir "$group/$idname"
+fi
+
+if [ ! -d "$group/$idname/$version" ]
+then
+    mkdir "$group/$idname/$version"
+fi
+
+cp target/*.jar "$group/$idname/$version"
+cp pom.xml $group/$idname/$version/$idname-$version.pom

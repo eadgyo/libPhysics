e34bcd15194caddaffae5a09decc1f8572887a9f
ronan <ronanjamet.h@gmail.com>
2016-06-03 01:32:53 +0200

    Readding Circle pre collision detection

--- src/main/java/org/cora/physics/collision/ContactGenerator.java
+++ src/main/java/org/cora/physics/collision/ContactGenerator.java
@@ -52,9 +52,7 @@ public class ContactGenerator
     public static boolean isColliding(Particle A, Particle B, Vector2D push,
             FloatA t)
     {
-        return CollisionDetector.isColliding(A.getForm(), B.getForm(),
-                A.getVelocity(), B.getVelocity(), push, t);
-        /*
+
         float dt = t.v;
         if (CollisionDetector.isColliding(A.getCircleBound(), B.getCircleBound(), A.getVelocity(), B.getVelocity(), push, t))
         {
@@ -63,7 +61,7 @@ public class ContactGenerator
             return CollisionDetector.isColliding(A.getForm(), B.getForm(),
                     A.getVelocity(), B.getVelocity(), push, t);
         }
-        return false;*/
+        return false;
     }
 
     public static boolean analyseContacts(Particle A, Particle B,
